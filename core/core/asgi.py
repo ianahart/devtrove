6e559df3db07053c0877c9b61aa06918e824e3bc
Ian Hart <ianhart7790@gmail.com>
2022-05-13 07:29:20 -0400

    deployment: toying with settings

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,12 +1,14 @@
 import os
+import django
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import chat.routing
-import invitation.routing
 from .channelsmiddleware import TokenAuthMiddleware
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+import chat.routing
+import invitation.routing
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
