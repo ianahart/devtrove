72425fc4a0b08ff302abadd35de6edb57c9b08f4
Ian Hart <ianhart7790@gmail.com>
2022-05-13 10:01:00 -0400

    deployment: playing around with settings for database

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -3,7 +3,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from .channelsmiddleware import TokenAuthMiddleware
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+
 import django
 django.setup()
 import chat.routing
